dddc472a453aa8d5b2c8b01304e269441d0b789f
Duncan Girauld Garcia <148366756+arkzxk@users.noreply.github.com>
2026-09-11 11:13:05 +0800

    Created customized card with guide

--- lib/main.dart
+++ lib/main.dart
@@ -3,37 +3,63 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: Text('Beautiful Card'),
+        title: Text('Design Challenge'),
       ),
       body: Center(
-        child: Card(
-          child: Column(
+        child: Container(
+          height: 330,
+          width: 300,
+          child: Card(
+            child: Column(
               children: [
                 Container(
-                  child: Image.asset('assets/24864.jpg', width: 300, height: 300,),
-                ),
-                Container(
-                  child:
-                    Text('Duncan Garcia          P30.00',
-                    style:
-                      TextStyle(
-                        fontSize: 22,
-                    ),
+                  height: 200,
+                  width: 300,
+                  child: Image.asset(
+                    'assets/24864.jpg',
+                    fit: BoxFit.cover,
                   ),
                 ),
                 Container(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print('Item purchased.');
-                    },
-                    child: Text('Purchase now!'),
+                  padding: EdgeInsets.only(
+                    top: 8.0,
+                    left: 8.0,
+                    right: 8.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Duncan Garcia',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Text(
+                            '\$30.25',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        'Handsome, charming, a leader',
+                        style: TextStyle(color: Colors.black54),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('ADD TO CART'),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
         ),
+      ),
       ),
     ),
   );
